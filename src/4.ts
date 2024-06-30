1c6653f1b8c2f627b5960ad7b6d60423570144d5
Vlad P <vlad@pysarenko.com>
2024-06-30 03:13:31 +0200

    HW done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random() * 1000;
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: Person[] = [];
+  public door: boolean = false;
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Welcome home. There are now ' + this.tenants.length + ' tenants');
+      return;
+    }
+    console.log("Door closed, can't come in");
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door opened');
+      return;
+    }
+    console.log('Wrong key');
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
